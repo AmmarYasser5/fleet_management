f3a8e8050ac211f4c931a4d4d68b6358a3773927
AmmarYasser5 <166479667+AmmarYasser5@users.noreply.github.com>
2024-12-17 20:29:10 +0200

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using AdminDashboard3.Models;
+using AdminDashboard3.Models.models;
+using AdminDashboard3.Models;
+using System.Collections.Generic;
+
+namespace AdminDashboard3.NewFolder
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+    }
+}
+
---
+++ DbContext.cs
@@ -0,0 +1,13 @@
+﻿using AdminDashboard3.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<Driver> Drivers { get; set; }
+}
